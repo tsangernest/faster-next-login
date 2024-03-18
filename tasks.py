b2c7700d0073b8f39999a45629c40f6b06230499
p14s <tsangernest@hotmail.com>
2024-03-18 12:32:55 -0500

    formatting

--- tasks.py
+++ tasks.py
@@ -1,38 +1,46 @@
 from invoke import task
 
 
+_CONTAINER_NAME = "fastapi"
+
+
 @task
 def logs(c):
-    cmd = f"docker logs -f fastapi"
+    cmd = f"docker logs -f {_CONTAINER_NAME}"
     c.run(cmd)
 
 
 @task
-def exec(c):
-    cmd = f"docker exec -it fastapi /bin/bash"
+def exec(c, target: str = f"/bin/bash"):
+
+    cmd = f"docker exec -it {_CONTAINER_NAME} {target}"
     c.run(cmd, pty=True)
 
 
 @task
-def startapp(c):
+def startapp(c, rebuild=True):
+
+    if rebuild:
+        build(c)
+
     cmd = (f"docker run "
            f"--detach "
-           f"--name fastapi "
+           f"--name {_CONTAINER_NAME} "
            f"-p 80:80 fastapi:latest")
     c.run(cmd)
 
 
 @task
 def down(c):
-    cmd = f"docker kill fastapi"
+    cmd = f"docker kill {_CONTAINER_NAME}"
     c.run(cmd)
 
-    rm_cmd = f"docker rm fastapi"
+    rm_cmd = f"docker rm {_CONTAINER_NAME}"
     c.run(rm_cmd)
 
 
 @task
 def build(c):
-    cmd = f"docker build -t fastapi ."
+    cmd = f"docker build -t {_CONTAINER_NAME} ."
     c.run(cmd)
 
